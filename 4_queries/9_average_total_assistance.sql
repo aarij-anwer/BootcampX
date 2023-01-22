0762bc92cc242ccff782425c0b47901fa2bcf633
Aarij Anwer <aarij.anwer@gmail.com>
2023-01-22 15:38:01 -0500

    queries for totals/avgs/ass

---
+++ 4_queries/9_average_total_assistance.sql
@@ -0,0 +1,8 @@
+SELECT avg(total_assistance.total_duration)
+FROM (
+  SELECT cohorts.name as name, sum(completed_at - started_at) as total_duration
+  FROM assistance_requests
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  GROUP BY cohorts.name
+  ORDER BY total_duration) total_assistance;
